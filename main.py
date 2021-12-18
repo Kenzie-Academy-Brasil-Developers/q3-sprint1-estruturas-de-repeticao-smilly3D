ae5d9ffa6ec52183418679e417f1e78d741148ef
Smilly Delmondes <smilly.3d@gmail.com>
2021-12-18 10:08:06 -0300

    Has done

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-# Código do dev aqui
+def corresponding_parenthesis(text):
+    left_bracket = 0
+    right_bracket = 0
+    sorted_characters = sorted(str(text))
+    print(sorted_characters)
+
+    for item in sorted_characters:
+        if item == "(":
+            left_bracket += 1
+        elif item == ")":
+            right_bracket += 1
+    if left_bracket > right_bracket:
+        return f'{(left_bracket - right_bracket)* "("}'
+    elif left_bracket < right_bracket:
+        return f'{(right_bracket - left_bracket)* ")"}'
+    else:
+        return ""
+
+
+def remove_more_than_two_repetitions(text):
+    output = ""
+    for index, ele in enumerate(text):
+        if not(ele == text[index-2] and ele == text[index-1]):
+            output += ele
+    return output
+
+    
+
+
+
